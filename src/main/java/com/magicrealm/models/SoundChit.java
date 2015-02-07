207990d0b9b9014b58ed62e73fcce24f6916cf69
zach-chai <zacharychai@cmail.carleton.ca>
2015-02-07 16:24:46 -0500

    sound and warning chits

---
+++ src/main/java/com/magicrealm/models/SoundChit.java
@@ -0,0 +1,8 @@
+package com.magicrealm.models;
+
+public class SoundChit {
+	
+	private int clearing;
+	private enum sound {howl, flutter, roar, patter, slither};
+
+}
---
+++ src/main/java/com/magicrealm/models/WarningChit.java
@@ -0,0 +1,8 @@
+package com.magicrealm.models;
+
+public class WarningChit {
+	
+	private enum tile {valley, woods, caves, mountains}
+	private enum warning {smoke, ruins, dank, bones}
+
+}
