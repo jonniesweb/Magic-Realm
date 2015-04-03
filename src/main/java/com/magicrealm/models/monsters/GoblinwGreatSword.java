ba66c94d9055139dbd00127abfbaadc20090a731
iain-wong <iainwong@cmail.carleton.ca>
2015-04-02 21:46:17 -0400

    Add GoblinwGreatSword Monster Class

---
+++ src/main/java/com/magicrealm/models/monsters/GoblinwGreatSword.java
@@ -0,0 +1,38 @@
+package com.magicrealm.models.monsters;
+
+import com.magicrealm.models.Weight;
+import com.magicrealm.models.weapons.GreatSword;
+
+public class GoblinwGreatSword extends MRMonster {
+	
+	public GoblinwGreatSword ()
+	{
+		super ( monster.goblinwGreatSword );
+		
+		setAttentionChit ( true );
+		
+		/* Set Object Related Attributes **************************************/
+		setName 		 ( "goblinwGreatSword" );
+		setDescription 	 ( "This peon is a little bit more manageable than" +
+						   " it's pesky axe weilding brother. As his sword" +
+				           " pretty much compensates for both his wit and"  +
+						   " character, one must only mind the first few"   +
+				           " initial swings, before the enemy will get"     +
+						   " confused and then begin to attack his own"     +
+				           " kind. Unusual behaviour, good luck traveller.");
+		setVulnerability ( Weight.LIGHT );
+		setFame			 ( 1 );
+		setNotoriety	 ( 1 );
+		
+		GreatSword greatSword = new GreatSword ();
+		addWeapon 	   ( greatSword );
+		activateWeapon ( greatSword );
+	}
+	
+	@Override
+	public String getImageName ()
+	{
+		return "theNameOfThisImageFile";
+	}
+
+}
