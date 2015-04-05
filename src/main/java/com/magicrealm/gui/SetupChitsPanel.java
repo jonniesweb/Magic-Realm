691e780472b95a9a9b3d481653d6c54ef75705d7
Jon Simpson <jon@jonsimpson.ca>
2015-04-05 13:11:28 -0400

    More progress on the SetupChitsPanel

--- src/main/java/com/magicrealm/gui/SetupChitsPanel.java
+++ src/main/java/com/magicrealm/gui/SetupChitsPanel.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,7 +21,11 @@ import net.miginfocom.swing.MigLayout;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.magicrealm.models.chits.ClearingMapChit;
+import com.magicrealm.models.chits.SiteChit;
+import com.magicrealm.models.chits.WarningChit;
 import com.magicrealm.models.chits.SiteChit.site;
+import com.magicrealm.models.chits.SoundChit;
 import com.magicrealm.models.chits.SoundChit.sound;
 import com.magicrealm.models.chits.WarningChit.warning;
 import com.magicrealm.models.tiles.GameTile.TileType;
@@ -30,55 +35,103 @@ public class SetupChitsPanel extends JPanel {
 	private final Log log = LogFactory.getLog(SetupChitsPanel.class);
 	
 	private final JPanel tileListPanel = new JPanel();
+	private final JPanel lostCityCastlePanel = new JPanel();
 	private List<TileRow> tileRows = new ArrayList<>();
+	private List<JTextField> lostCastleRows = new ArrayList<>();
+	private List<JTextField> lostCityRows = new ArrayList<>();
+	private final JLabel siteChitDescriptionLabel = new JLabel(getSiteChitsDescription());
+	private final JLabel soundChitDescriptionLabel = new JLabel(getSoundChitsDescription());
+	private final JLabel warningChitDescriptionLabel = new JLabel(getWarningChitsDescription());
+	private final JPanel descriptionLabelPanel = new JPanel();
+	private final JButton btnSubmit = new JButton("Submit");
 	
 	/**
 	 * Create the panel.
 	 */
 	public SetupChitsPanel() {
-		setLayout(new MigLayout("", "[300.00px,grow][100px,grow][grow]", "[][][grow]"));
+		/*
+		 * Setup ui components
+		 */
+		setLayout(new MigLayout("", "[300px,grow][100px,grow][grow]", "[][]"));
 		
-		add(tileListPanel, "cell 0 0,grow");
+		// setup tile site, sound and warming chit input
+		add(tileListPanel, "cell 0 0 1 2,grow");
 		tileListPanel.setLayout(new BoxLayout(tileListPanel, BoxLayout.Y_AXIS));
 		tileListPanel.setBorder(new LineBorder(Color.BLACK));
 		
+		tileListPanel.add(new JLabel("site/sound chit       warning Chit"));
 		for (TileType tile : TileType.values()) {
 			TileRow tileRow = new TileRow(tile);
 			tileRows.add(tileRow);
 			tileListPanel.add(tileRow);
 		}
 		
+		// setup lost city and lost castle chit input
+		add(lostCityCastlePanel, "cell 1 0,growx,aligny top");
+		lostCityCastlePanel.setLayout(new BoxLayout(lostCityCastlePanel, BoxLayout.Y_AXIS));
+		tileListPanel.setBorder(new LineBorder(Color.BLACK));
+		
+		lostCityCastlePanel.add(new JLabel("Lost City site/sound chit"));
+		for (int i = 0; i < 5; i++) {
+			JTextField textField = new JTextField();
+			textField.setColumns(10);
+			lostCityCastlePanel.add(textField);
+			lostCityRows.add(textField);
+		}
+		
+		lostCityCastlePanel.add(new JLabel("Lost Castle site/sound chit"));
+		for (int i = 0; i < 5; i++) {
+			JTextField textField = new JTextField();
+			textField.setColumns(10);
+			lostCityCastlePanel.add(textField);
+			lostCastleRows.add(textField);
+		}
+		lostCityCastlePanel.add(new JLabel("<html>The Lost City and Lost Castle"
+				+ " site/sound chits specified above will be added to the tile"
+				+ " that has the lost_city and lost_castle site chit assigned"
+				+ " to it, respectively.</html>"));
+		
+		// add description labels for the chit inputs
+		add(descriptionLabelPanel, "cell 2 0,growx,aligny top");
+		descriptionLabelPanel.setLayout(new MigLayout("", "[grow]", "[][][grow]"));
+		descriptionLabelPanel.add(siteChitDescriptionLabel, "cell 0 0");
+		descriptionLabelPanel.add(soundChitDescriptionLabel, "cell 0 1");
+		descriptionLabelPanel.add(warningChitDescriptionLabel, "cell 0 2");
+		
+		// add a submit button
+		add(btnSubmit, "cell 2 1,alignx right,aligny bottom");
 	}
-	
-	// do lost city and lost castle
 	
 	private static String getSiteChitsDescription() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("Avaliable Site chits: ");
+		sb.append("<html>Avaliable Site chits: ");
 		for (site site : site.values()) {
-			sb.append("\n");
+			sb.append("<br>");
 			sb.append(site);
 		}
+		sb.append("</html>");
 		return sb.toString();
 	}
 	
 	private static String getSoundChitsDescription() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("Avaliable Sound chits: ");
+		sb.append("<html>Avaliable Sound chits: ");
 		for (sound sound : sound.values()) {
-			sb.append("\n");
+			sb.append("<br>");
 			sb.append(sound);
 		}
+		sb.append("</html>");
 		return sb.toString();
 	}
 	
 	private static String getWarningChitsDescription() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("Avaliable Warning chits: ");
+		sb.append("<html>Avaliable Warning chits: ");
 		for (warning warning : warning.values()) {
-			sb.append("\n");
+			sb.append("<br>");
 			sb.append(warning);
 		}
+		sb.append("</html>");
 		return sb.toString();
 	}
 	
@@ -86,8 +139,8 @@ public class SetupChitsPanel extends JPanel {
 	 * Get all warning chits assigned to the tiles
 	 * @return
 	 */
-	public Map<TileType, warning> getWarningChits() {
-		EnumMap<TileType, warning> warningMap = new EnumMap<>(TileType.class);
+	public Map<TileType, WarningChit> getWarningChits() {
+		EnumMap<TileType, WarningChit> warningMap = new EnumMap<>(TileType.class);
 		
 		for (TileRow row : tileRows) {
 			String warningChitText = row.getWarningChitText();
@@ -95,61 +148,91 @@ public class SetupChitsPanel extends JPanel {
 			if (warningChitText.isEmpty())
 				continue;
 			
-			warningMap.put(row.getTileType(),
-					warning.valueOf(warningChitText));
+			try {
+				warningMap.put(row.getTileType(), new WarningChit(warning.valueOf(warningChitText)));
+			} catch (IllegalArgumentException e) {
+				log.error("unable to parse warning chit from input: " + warningChitText);
+			}
 		}
 		return warningMap;
 	}
 	
 	/**
-	 * Get all site chits assigned to the tiles
+	 * Get all site and sound chits assigned to the tiles
 	 * @return
 	 */
-	public Map<TileType, site> getSiteChits() {
-		EnumMap<TileType, site> siteMap = new EnumMap<>(TileType.class);
+	public Map<TileType, ClearingMapChit> getClearingChits() {
+		EnumMap<TileType, ClearingMapChit> chitMap = new EnumMap<>(TileType.class);
 		
 		for (TileRow row : tileRows) {
-			String mapChitText = row.getMapChitText();
+			String chitText = row.getMapChitText();
 			
-			if (mapChitText.isEmpty())
+			if (chitText.isEmpty())
 				continue;
 			
+			// try parsing a site chit
 			try {
-				siteMap.put(row.getTileType(), site.valueOf(mapChitText));
+				chitMap.put(row.getTileType(), new SiteChit(site.valueOf(chitText)));
 			} catch (IllegalArgumentException e) {
-				log.warn("unable to parse site chit from input: " + mapChitText);
+				
+				// try parsing a sound chit
+				try {
+					chitMap.put(row.getTileType(), new SoundChit(sound.valueOf(chitText)));
+				} catch (IllegalArgumentException e1) {
+					
+					// no luck
+					log.warn("unable to parse sound or site chit from input: " + chitText);
+				}
 			}
 		}
-		return siteMap;
+		return chitMap;
 	}
 	
-	/**
-	 * Get all sound chits assigned to the tiles
-	 * @return
-	 */
-	public Map<TileType, sound> getSoundChits() {
-		EnumMap<TileType, sound> soundMap = new EnumMap<>(TileType.class);
+	public List<ClearingMapChit> getLostCityChits() {
+		ArrayList<String> list = new ArrayList<String>();
+		for (JTextField jTextField : lostCityRows) {
+			list.add(jTextField.getText());
+		}
 		
-		for (TileRow row : tileRows) {
-			String mapChitText = row.getMapChitText();
+		return getClearingChits(list);
+	}
+	
+	public List<ClearingMapChit> getLostCastleChits() {
+		ArrayList<String> list = new ArrayList<String>();
+		for (JTextField jTextField : lostCastleRows) {
+			list.add(jTextField.getText());
+		}
+		
+		return getClearingChits(list);
+	}
+	
+	private List<ClearingMapChit> getClearingChits(List<String> rows) {
+		ArrayList<ClearingMapChit> list = new ArrayList<>();
+		for (String text : rows) {
 			
-			if (mapChitText.isEmpty())
+			if (text.isEmpty())
 				continue;
 			
+			// try parsing it as a site chit
 			try {
-				soundMap.put(row.getTileType(), sound.valueOf(mapChitText));
+				list.add(new SiteChit(site.valueOf(text)));
 			} catch (IllegalArgumentException e) {
-				log.warn("unable to parse sound chit from input: " + mapChitText);
+				
+				// try parsing it as a sound chit
+				try {
+					list.add(new SoundChit(sound.valueOf(text)));
+				} catch (IllegalArgumentException e1) {
+					
+					// no dice
+					log.error("unable to parse a site or sound chit from the string: " + text);
+				}
 			}
 		}
-		return soundMap;
+		
+		return list;
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(getSiteChitsDescription());
-		System.out.println(getSoundChitsDescription());
-		System.out.println(getWarningChitsDescription());
-		
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
@@ -163,11 +246,16 @@ public class SetupChitsPanel extends JPanel {
 		
 	}
 	
+	/**
+	 * Helper class for displaying a row consisting of a label for showing the
+	 * tile type, a text field for specifying the map chit, and a text field for
+	 * specifying the warning chit.
+	 */
 	private class TileRow extends JPanel {
 		private JTextField mapChit;
 		private JTextField warningChit;
 		private TileType tileType;
-
+		
 		public TileRow(TileType tileType) {
 			this.tileType = tileType;
 			setLayout(new FlowLayout());
@@ -186,7 +274,7 @@ public class SetupChitsPanel extends JPanel {
 		public TileType getTileType() {
 			return tileType;
 		}
-
+		
 		public String getMapChitText() {
 			return mapChit.getText();
 		}
